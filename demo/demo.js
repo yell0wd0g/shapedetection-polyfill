4b073001403e571c808f6dee17a4efb40a1ffe84
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-02-06 20:44:47 -0800

    writing results in footer

--- demo/demo.js
+++ demo/demo.js
@@ -10,19 +10,21 @@ urlSelect.onchange = () => {
 
   img.onload = function() {
 
-    //var footer = document.getElementsByTagName('footer')[0];
+    var footer = document.getElementsByTagName('footer')[0];
 
     var bc = new BarcodeDetector();
 
     bc.detect(img)
         .then((barcodes) => {
    	      for(var i = 0; i < barcodes.length; i++) {
-            //footer.innerHTML = '+ found: ' + barcodes[i].rawValue + '\n';
+            if (footer !== 'undefined')
+              footer.innerHTML = '+ found: ' + barcodes[i].rawValue + '\n';
             console.log('Found sth: ' + barcodes[i].rawValue + '\n');
           }
         })
         .catch( (error) => {
-          //footer.innerHTML = 'detection failed: ' + error;
+          if (footer !== 'undefined')
+            footer.innerHTML = 'detection failed: ' + error;
           console.error(error);
         });
   }
