fb3be85f530cf6062b35c461c78deebbece37402
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-04-28 11:09:21 -0700

    fix webpack config file (https://github.com/webpack/webpack/issues/4549) and versioning

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './demo/demo.js',
   output: {
-    path: './demo',
+    path: path.resolve(__dirname, 'demo'),
     filename: 'demo.bundle.js'
   }
 };
